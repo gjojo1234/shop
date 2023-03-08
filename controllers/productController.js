0a646cd9df6f6c266ad91e1433b477047bd159e0
gjojo1234 <gensor1234@gmail.com>
2023-03-08 21:30:13 +0100

    update 4

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,7 +2,7 @@ import Product from "../models/Product.js";
 
 const getAllProducts = async (req, res) => {
   const product = await Product.find();
-  res.status(200).json({ product });
+  res.status(200).json(product);
 };
 const getFilterProducts = async (req, res) => {
   try {
